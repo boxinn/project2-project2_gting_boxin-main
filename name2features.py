6dadd6e95ae539d20314012dacfd1aa5ed803ab6
boxin <92691567+boxinn@users.noreply.github.com>
2022-02-18 19:41:16 -0600

    Update name2features.py

--- name2features.py
+++ name2features.py
@@ -10,20 +10,20 @@ def name2features(name):
     But every name must generate the same length feature. 
     """
     
-    d = 256 # number of hashing buckets 128
+    d = 20 # number of hashing buckets 128
     v = np.zeros(d)
     name=name.lower() 
     
     # hash prefixes & suffixes - alexander -> [prefixa, prefixal, prefixale, suffixr, suffixer, suffixder]
     
-    prefix_max = 15  #3
+    prefix_max = 25  #3
     for m in range(prefix_max):
         prefix_string='prefix'+name[0:min(m+1,len(name))]
         random.seed(prefix_string)
         prefix_index = int(random.randint(0,d-1))
         v[prefix_index] = 1
     
-    suffix_max = 13  #3
+    suffix_max = 23  #3
     for m in range(suffix_max):
         suffix_string='suffix'+name[-1:-min(m+2,len(name)+1):-1]
         random.seed(suffix_string)
